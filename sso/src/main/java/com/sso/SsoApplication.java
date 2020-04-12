1fcebf7f94ec10fc5e70769c7f6dac496921f77b
kongchao <kongchao507@qq.com>
2020-04-12 15:29:04 +0800

    ssoServer的模块添加了一个controller

--- sso/src/main/java/com/sso/SsoApplication.java
+++ sso/src/main/java/com/sso/SsoApplication.java
@@ -2,11 +2,13 @@ package com.sso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 /**
  * @author 孔超
  */
 @SpringBootApplication
+@EnableAuthorizationServer
 public class SsoApplication {
 
     public static void main(String[] args) {
