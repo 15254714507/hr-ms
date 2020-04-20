58621e97be904d839a5250752377b9f536e6a312
kongchao <kongchao507@qq.com>
2020-04-21 01:40:06 +0800

    添加Result

---
+++ api/src/main/java/com/hrms/api/until/Result.java
@@ -0,0 +1,43 @@
+package com.hrms.api.until;
+
+import lombok.Data;
+
+/**
+ * 返回给前端封装的对象
+ *
+ * @author 孔超
+ * @date 2020/4/13 23:07
+ */
+@Data
+public class Result {
+    /***
+     * 是否成功的标志
+     */
+    private Integer code;
+    /**
+     * 返回前端的信息提示
+     */
+    private String msg;
+
+    /***
+     * 返回的对象
+     */
+    private Object object;
+
+    public Result(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Result(Integer code, Object object) {
+        this.code = code;
+        this.object = object;
+    }
+
+    public Result(Integer code, String msg, Object object) {
+        this.code = code;
+        this.msg = msg;
+        this.object = object;
+    }
+
+}
